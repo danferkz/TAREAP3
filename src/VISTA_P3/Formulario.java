4d6d794fd65f6aa1107649c2ea9146c5614c6fe0
vaniavp <vaniavapa2015@hotmail.com>
2023-06-03 00:00:26 -0500

    hi

--- src/VISTA_P3/Formulario.java
+++ src/VISTA_P3/Formulario.java
@@ -13,7 +13,7 @@ import javax.swing.table.DefaultTableModel;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
-import MODELO_P3.*;
+import MODELO_P3.*; 
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
